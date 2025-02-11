d1bfaef1a60bd12dcd86fa1cbfac132188e99d41
kunj <kunjkariya@gmail.com>
2025-02-11 12:39:45 -0500

    Initial Commit

---
+++ ColorMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Zenaliba Rana on 2025-02-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ColorMakerTests/ColorMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMakerTests.swift
+//  ColorMakerTests
+//
+//  Created by Zenaliba Rana on 2025-02-11.
+//
+
+import Testing
+@testable import ColorMaker
+
+struct ColorMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
